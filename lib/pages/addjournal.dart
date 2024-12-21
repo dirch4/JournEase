df34c0a97ddbcf303a9122c22eac0b7244674547
Dimas Nurcahya <dimasnurcahya478@gmail.com>
2024-12-21 09:56:43 +0700

    page: update page add journal

--- lib/pages/addjournal.dart
+++ lib/pages/addjournal.dart
@@ -27,41 +27,78 @@ class Addjournal extends StatelessWidget {
           Container(
             color: Appcolors.primary,
           ),
+          Positioned(
+            top: -40,
+            left: -40,
+            child: Transform.rotate(
+              angle: -0.10,
+              child: Image.asset(
+                'assets/iconbg1.png',
+                width: 300,
+                height: 300,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: -50,
+            right: -30,
+            child: Transform.rotate(
+              angle: 160,
+              child: Image.asset(
+                'assets/iconbg1.png',
+                width: 200,
+                height: 200,
+              ),
+            ),
+          ),
           Center(
             child: Container(
               alignment: Alignment.center,
-              height: 550,
+              height: 500,
               width: 400,
               decoration: BoxDecoration(
                   color: Appcolors.secondary,
                   borderRadius: BorderRadius.all(Radius.circular(20.0))),
               child: Column(
                 children: [
-                  const SizedBox(height: 20.0,),
-                  Text("Tambah Jurnal", style: GoogleFonts.poppins(
-                          textStyle: const TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "Tambah Jurnal",
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.all(30.0),
                     child: Column(
+                      
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text("Judul : "),
                         TextField(
                           decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
                             border: OutlineInputBorder(),
-                            hintText: 'Enter a search term',
+                            hintText: 'Masukkan judul',
                           ),
                         ),
-                        Text("Ceritakan Harimu : "),
                         const SizedBox(height: 10.0),
+                        Text("Ceritakan Harimu : "),
+                        const SizedBox(height: 5.0),
                         TextField(
+                          maxLines: 7,
                           decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
                             border: OutlineInputBorder(),
-                            hintText: 'Enter a search term',
+                            hintText: 'Ceritakan pengalaman hari ini',
                           ),
                         ),
                       ],
@@ -75,11 +112,11 @@ class Addjournal extends StatelessWidget {
                         Navigator.pop(context);
                       },
                       style: OutlinedButton.styleFrom(
-                        backgroundColor: Colors.white,
+                        backgroundColor: Appcolors.primary,
                         side: const BorderSide(color: Colors.black, width: 1),
                         padding: const EdgeInsets.symmetric(
                           vertical: 20.0,
-                          horizontal: 20.0,
+                          horizontal: 110.0,
                         ),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(8.0),
@@ -101,30 +138,7 @@ class Addjournal extends StatelessWidget {
               ),
             ),
           ),
-          Positioned(
-            bottom: 30,
-            left: -40,
-            child: Transform.rotate(
-              angle: -0.10,
-              child: Image.asset(
-                'assets/iconbg1.png',
-                width: 300,
-                height: 300,
-              ),
-            ),
-          ),
-          Positioned(
-            top: -50,
-            right: 30,
-            child: Transform.rotate(
-              angle: 160,
-              child: Image.asset(
-                'assets/iconbg1.png',
-                width: 200,
-                height: 200,
-              ),
-            ),
-          ),
+          
         ],
       ),
     );
